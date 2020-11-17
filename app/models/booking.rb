eb3624eafb0ebeac304a8bf31100ff9fcd1ac16d
Georgi Ankov <georgi.ankov@yahoo.co.uk>
2020-11-17 12:06:08 +0000

    Bookings MVC

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :customer, :class_name => "User"
   belongs_to :plane
 
-
-
+  validate :overlapping
+    private
+    def overlapping
+    if Booking.where('? <  end_date and ? > start_date', self.start_date, self.end_date).any?
+        errors.add(:end_date, 'it overlaps another')
+  end
 end
