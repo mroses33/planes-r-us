2403bd7b14cb87691ce28df2c5329ef076e89cbe
Agata Filus <agatafilus@gmail.com>
2020-11-17 17:19:56 +0000

    Plane model validations

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -2,5 +2,8 @@ class Plane < ApplicationRecord
   belongs_to :owner, :class_name => "User"
 
   has_many :bookings
+  validates :name, length: {minimum: 2}, presence: true
+  validates :description, length: {minimum: 10}, presence: true
+  validates :price, presence: true
 end
 
