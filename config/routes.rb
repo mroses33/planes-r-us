596d225a4033b0460d9f5d088ed1709125f5b088
Sophia Eady <s.eady@outlook.com>
2020-11-19 12:09:39 +0000

    correct routes added, basic user page also

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'planes/index'
-  get 'planes/show'
-  get 'planes/new'
-  get 'planes/create'
   devise_for :users
   root to: 'pages#home'
   resources :planes, only: [:new, :create, :index, :show] do
     resources :bookings, only: [:new, :create, :index, :show]
   end
+  resources :users, only: [:show]
 end
